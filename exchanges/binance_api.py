b0bb9dd6388adfaec3c29ac84431df768311b6c6
danielhuri <danielhuri8@gmail.com>
2022-11-23 21:49:49 +0200

    binance margin sell

--- exchanges/binance_api.py
+++ exchanges/binance_api.py
@@ -5,8 +5,10 @@ BINANCE_NAME = "BINANCE"
 
 class BinanceAPI():
     def __init__(self, api_key, api_secret) -> None:
+        self.base_asset = "BUSD"
         self.client = Client(api_key, api_secret)
-        print("Connected to binance API with permissions:\n{}".format(self.client.get_account_api_permissions()))
+        self.balance = self.client.get_asset_balance(asset=self.base_asset)        
+        print("Connected to binance API with {} balance: {}".format(self.base_asset, self.balance))
 
     def get_order_book(self, symbol):
         return self._get_order_book(symbol)
@@ -17,12 +19,25 @@ class BinanceAPI():
     def get_ask_order_book(self, orderbook):                
         return orderbook["asks"]
 
-    def create_order(self, symbol, quantity ,side=Client.SIDE_BUY, type=Client.ORDER_TYPE_MARKET):                                
+    def create_order(self, symbol, quantity ,side=Client.SIDE_BUY, type=Client.ORDER_TYPE_MARKET):  
+        # Just remeber - “quantity” is used to describe base asset (left of the symbol) and “quoteOrderQty” for the quote(right) asset        
         return self.client.create_order(
                 symbol=symbol,
                 side=side,
                 type=type,
                 quantity=quantity)
+
+    def transfer_spot_to_margin(self, amount, asset=None):        
+        self.client.transfer_spot_to_margin(asset=asset or self.base_asset, amount=amount) 
+    
+    def create_margin_order(self, symbol, quantity ,side=Client.SIDE_SELL, type=Client.ORDER_TYPE_MARKET):
+        asset = symbol.split(self.base_asset)[0]        
+        self.client.create_margin_loan(asset=asset,amount=quantity)
+        self.client.create_margin_order(symbol=symbol,
+                side=side,
+                type=type,
+                quantity=quantity)        
+        return 
 
     def withdraw(self, asset, address, amount):
         try:
