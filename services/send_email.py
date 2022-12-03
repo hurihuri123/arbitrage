a48ed79cc90c013e07b48b32ba13adcc5a824185
danielhuri <danielhuri8@gmail.com>
2022-12-03 18:10:28 +0200

    new email api

---
+++ services/send_email.py
@@ -0,0 +1,40 @@
+import smtplib
+
+def sendEmail(title, contect):
+    # =============================================================================
+    # SET EMAIL LOGIN REQUIREMENTS
+    # =============================================================================
+    gmail_user = 'talhurishared@gmail.com'
+    gmail_app_password = 'djodbbypqiszqtxg'
+
+    # =============================================================================
+    # SET THE INFO ABOUT THE SAID EMAIL
+    # =============================================================================
+    sent_from = gmail_user
+    sent_to = ['talyacobi123@gmail.com', 'danielhuri8@gmail.com']
+    sent_subject = title
+    sent_body = contect
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+
+    # =============================================================================
+    # SEND EMAIL OR DIE TRYING!!!
+    # Details: http://www.samlogic.net/articles/smtp-commands-reference.htm
+    # =============================================================================
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_app_password)
+        server.sendmail(sent_from, sent_to, email_text)
+        server.close()
+
+        print('Email sent!')
+    except Exception as exception:
+        print("Error: %s!\n\n" % exception)
