904174e3b3e8bbb195586a2ff342d612bb3a1bb5
danielhuri <danielhuri8@gmail.com>
2023-01-03 21:04:28 +0200

    use margin binanace list

--- main.py
+++ main.py
@@ -42,16 +42,16 @@ if __name__ == "__main__":
     # print("algo 2")
     # print(arbitrage.temp_shuld_take_arbitrage2(binance,kucoin,symbol))
 
-    symbols = []
-    currencies = binance.get_all_coins()             
-    for currency in currencies:
-        symbol = currency["symbol"]
-        if symbol and "USDT" in symbol:
-            symbols.append(symbol)   
+    binance_margin_symbols = ['BNBUSDT', 'BTCUSDT', 'ETHUSDT', 'TRXUSDT', 'XRPUSDT', 'EOSUSDT', 'LINKUSDT', 'ONTUSDT', 'ADAUSDT', 'ETCUSDT', 'LTCUSDT', 'XLMUSDT', 'XMRUSDT', 'NEOUSDT', 'ATOMUSDT', 'DASHUSDT', 'ZECUSDT', 'MATICUSDT', 'BATUSDT', 'IOSTUSDT', 'VETUSDT', 'QTUMUSDT', 'IOTAUSDT', 'XTZUSDT', 'BCHUSDT', 'RVNUSDT', 'BUSDUSDT', 'ZILUSDT', 'ONEUSDT', 'ANKRUSDT', 'TFUELUSDT', 'IOTXUSDT', 'HBARUSDT', 'FTMUSDT', 'SXPUSDT', 'DOTUSDT', 'ALGOUSDT', 'THETAUSDT', 'COMPUSDT', 'KNCUSDT', 'KAVAUSDT', 'DOGEUSDT', 'WAVESUSDT', 'MKRUSDT', 'SNXUSDT', 'CRVUSDT', 'SUSHIUSDT', 'UNIUSDT', 'MANAUSDT', 'JSTUSDT', 'AVAXUSDT', 'NEARUSDT', 'FILUSDT', 'TRBUSDT', 'RSRUSDT', 'AAVEUSDT', 'SANDUSDT', 'CHZUSDT', 'ARPAUSDT', 'COTIUSDT', 'FETUSDT', 'TROYUSDT', 'CHRUSDT', 'NMRUSDT', 'GRTUSDT', 'STPTUSDT', 'LRCUSDT', 'KSMUSDT', 'ROSEUSDT', 'REEFUSDT', 'STXUSDT', 'ENJUSDT', 'RUNEUSDT', 'SKLUSDT', 'INJUSDT', 'OGNUSDT', 'EGLDUSDT', '1INCHUSDT', 'DODOUSDT', 'LITUSDT', 'CAKEUSDT', 'SOLUSDT', 'LINAUSDT', 'MDXUSDT', 'SUPERUSDT', 'GTCUSDT', 'PUNDIXUSDT', 'AUDIOUSDT', 'BONDUSDT', 'SLPUSDT', 'POLSUSDT', 'PONDUSDT', 'NULSUSDT', 'TVKUSDT', 'ATAUSDT', 'DENTUSDT', 'ERNUSDT', 'ARUSDT', 'DYDXUSDT', 'UNFIUSDT', 'AXSUSDT', 'LUNAUSDT', 'SHIBUSDT', 'WINUSDT', 'ENSUSDT', 'ALICEUSDT', 'TLMUSDT', 'ICPUSDT', 'C98USDT', 'FLOWUSDT', 'BAKEUSDT', 'GALAUSDT', 'HIVEUSDT', 'DARUSDT', 'IDEXUSDT', 'MBOXUSDT', 'ANTUSDT', 'CLVUSDT', 'WAXPUSDT', 'KLAYUSDT', 'MINAUSDT', 'RNDRUSDT', 'JASMYUSDT', 'QUICKUSDT', 'LPTUSDT', 'AGLDUSDT', 'BICOUSDT', 'CTXCUSDT', 'DUSKUSDT', 'HOTUSDT', 'SFPUSDT', 'YGGUSDT', 'FLUXUSDT', 'ICXUSDT', 'CELOUSDT', 'BETAUSDT', 'BLZUSDT', 'MTLUSDT', 'PEOPLEUSDT', 'QNTUSDT', 'PYRUSDT', 'SUNUSDT', 'KEYUSDT', 'PAXGUSDT', 'WANUSDT', 'TWTUSDT', 'RADUSDT', 'QIUSDT', 'GMTUSDT', 'APEUSDT', 'KDAUSDT', 'MBLUSDT', 'API3USDT', 'CTKUSDT', 'NEXOUSDT', 'WOOUSDT', 'ASTRUSDT', 'GALUSDT', 'OPUSDT', 'REIUSDT', 'LEVERUSDT', 'LDOUSDT', 'FIDAUSDT', 'KMDUSDT', 'FLMUSDT', 'BURGERUSDT', 'AUCTIONUSDT', 'FIOUSDT', 'IMXUSDT', 'SPELLUSDT', 'STGUSDT', 'BELUSDT', 'WINGUSDT', 'AVAUSDT', 'DEXEUSDT', 'LUNCUSDT', 'SANTOSUSDT', 'EPXUSDT', 'HARDUSDT', 'DEGOUSDT', 'HIGHUSDT', 'GMXUSDT', 'LAZIOUSDT', 'PORTOUSDT', 'ACHUSDT', 'STRAXUSDT', 'FORTHUSDT', 'KP3RUSDT', 'BSWUSDT', 'REQUSDT', 'POLYXUSDT', 'APTUSDT', 'DATAUSDT', 'PHAUSDT', 'OSMOUSDT', 'GLMRUSDT', 'MASKUSDT']
+    # currencies = binance.get_all_coins()             
+    # for currency in currencies:
+    #     symbol = currency["symbol"]
+    #     if symbol and "USDT" in symbol:
+    #         symbols.append(symbol)   
 
     while True:
         try:
-            did_took_arbitrage = arbitrage.scan(symbols=symbols, exchange1=exchanges_dict["BINANCE"], exchange2=exchanges_dict["KUCOIN"]) 
+            did_took_arbitrage = arbitrage.scan(symbols=binance_margin_symbols, exchange1=exchanges_dict["BINANCE"], exchange2=exchanges_dict["KUCOIN"]) 
             if did_took_arbitrage:
                 break
             time.sleep(5)
